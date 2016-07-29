b615b206ae6d2eea92394ba2518e539eb70d21f9
wordyallen <joe.narvaez@gmail.com>
2016-07-29 12:25:19 -0400

    update cron to remove fixdata

--- cron_tasks.sh
+++ cron_tasks.sh
@@ -2,5 +2,4 @@
 
 cd /home/codeformiami/Dropbox/mia-councilmatic
 /home/codeformiami/Dropbox/mia-councilmatic/venv/lib/python3.4/python manage.py loaddata >> /tmp/chicago-loaddata.log 2>&1
-/home/codeformiami/Dropbox/mia-councilmatic/venv/lib/python3.4/python manage.py fixdata >> /tmp/chicago-loaddata.log 2>&1
 /home/codeformiami/Dropbox/mia-councilmatic/venv/lib/python3.4/python manage.py update_index --age=24
